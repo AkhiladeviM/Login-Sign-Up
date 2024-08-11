02d6b4a9a313cfcb30d74e69177fb648227a85fa
Akhiladevi M <akhiladevimaddipati@gmail.com>
2024-08-11 18:23:55 +0530

    main.5039ef67.css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
 
     <div className="App">
-      <link rel="stylesheet" href={`${process.env.PUBLIC_URL}/main.5039ef67.css`} />
-      <Router>
+      <Router basename="/loginSignUp">
         <Switch>
           <Route path="/" exact component={LoginForm} />
           <Route path="/signup" component={SignupForm} />
